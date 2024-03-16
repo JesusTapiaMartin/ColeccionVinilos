10112c372a842d5d8238d6be308f0b1b6d20e57a
JesusTM <jesustapiamartin@gmail.com>
2024-03-15 23:54:51 -0300

    Colección Vinilos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,92 +2,110 @@ import java.util.Scanner;
 
 public class Main {
 
-    // ===== ATRIBUTOS =====
-    private static String[][] coleccionVinilos = new String[3][100];
-    private static int contadorVinilos = 0;
+    // =============== ATRIBUTOS ===============
+    private static String[][]   coleccionVinilos    = new String[3][100];
+    private static int          contadorVinilos     = 0;
 
-    // ===== MÉTODOS =====
-    // ----- AGREGAR VINILO -----
+
+    // =============== MÉTODOS ===============
+
+    // ----- Main -----
+    public static void main(String[] args) {
+        menu();
+    }
+
+
+    // ----- Agregar Vinilo -----
     public static void agregarVinilo() {
         Scanner lector = new Scanner(System.in);
 
         System.out.println(" ----- AGREGAR VINILO ----- ");
         System.out.print  ("    Artista        : ");
         String artista      = lector.nextLine();
-
         System.out.print  ("    Disco          : ");
         String disco        = lector.nextLine();
-
         System.out.print  ("    Lanzamiento    : ");
         int lanzamiento     = lector.nextInt();
 
         coleccionVinilos[0][contadorVinilos] = artista;
         coleccionVinilos[1][contadorVinilos] = disco;
         coleccionVinilos[2][contadorVinilos] = String.valueOf(lanzamiento);
-
         contadorVinilos = contadorVinilos + 1;
-        System.out.println(" Vinilo agregado a la colección... ");
+        System.out.println(" Vinilo agregado a la colección... \n");
     }
 
 
-    // ----- MOSTRAR COLECCIÓN -----
-    public static void mostrarColeccion(){
-        System.out.println(" ----- COLECCIÓN DE VINILOS ----- ");
-        System.out.printf("%-20s %-20s %-20s%n", "Artista", "Disco", "Lanzamiento");
 
-        for (int y = 0; y < coleccionVinilos[0].length; y++) {
+    // ----- Mostrar Colección -----
+    public static void mostrarColeccion() {
+        System.out.println("        ----- COLECCIÓN DE VINILOS ----- ");
+        System.out.println("    Cantidad de vinilos : " + contadorVinilos());
+        System.out.println("    Espacios vacíos     : " + contadorEspaciosVacios());
+        System.out.println("\n");
+        System.out.printf("%-20s%-20s%-20s\n", "ARTISTA", "DISCO", "LANZAMIENTO");
+
+        for (int y = 0; y < contadorVinilos; y++) {
             for (int x = 0; x < coleccionVinilos.length; x++) {
-                System.out.printf("%-20s ", coleccionVinilos[x][y]);
+
+                String elemento = coleccionVinilos[x][y];
+                if (elemento != null) {
+                    System.out.printf("%-20s", elemento);
+                } else {
+                    System.out.printf("%-20s", ""); // Espacio en blanco
+                }
             }
             System.out.println();
         }
+        System.out.println("\n");
     }
 
 
-    // ----- BUSCAR VINILO -----
-    public static void buscarVinilo(){
+
+    // ----- Buscar Vinilo -----
+    public static void buscarVinilo() {
         Scanner lector = new Scanner(System.in);
 
         System.out.println(" ----- BUSCAR VINILO ----- ");
-        System.out.print  ("    Disco          : ");
+        System.out.print("    Disco          : ");
         String disco = lector.nextLine();
 
-        for (int x = 0; x < coleccionVinilos.length ; x++) {
-            for (int y = 0; y < coleccionVinilos[x].length ; y++) {
-                if(coleccionVinilos[x][y].toLowerCase() == disco.toLowerCase()){
-                    int columna = x;
-                    int fila    = y;
-                    System.out.println(" Vinilo " + disco + " en la columna " + x + " fila " + y );
-                } else {
-                    System.out.println(" El vinilo" + disco +  " no se encuentra en la colección");
+        for (int x = 0; x < coleccionVinilos.length; x++) {
+            for (int y = 0; y < coleccionVinilos[x].length; y++) {
+                String vinilo = coleccionVinilos[x][y];
+                if (vinilo != null && vinilo.toLowerCase().equals(disco.toLowerCase())) {
+                    System.out.println( " El Vinilo \"" + disco     + "\" se encuentra en la" +
+                                        "en la fila "   + y         + ".");
+                    return;
                 }
             }
         }
+
+        // El vinilo no se encontró en la colección
+        System.out.println(" El vinilo \"" + disco + "\" no se encuentra en la colección");
     }
 
 
-    // ----- CONTADOR VINILOS -----
-    public static int contadorVinilos(){
-        Scanner lector  = new Scanner(System.in);
-        int contador    = 0;
 
-        for (int x = 0; x < coleccionVinilos.length ; x++) {
-            for (int y = 0; y < coleccionVinilos[x].length ; y++) {
-                while (lector.hasNextLine()){
-                    contador = contador + 1;
-                }
+    // ----- Contador Vinilos -----
+    public static int contadorVinilos(){
+        int contador = 0;
+
+        for (int y = 0; y < coleccionVinilos[0].length; y++) {
+            if (coleccionVinilos[0][y] != null) {
+                contador++;
             }
         }
         return contador;
     }
 
-    // ----- CONTADOR ESPACIOS VACÍOS -----
-    public static int contadorEspaciosVacios(){
-        int contador = contadorVinilos();
-        int contadorEspaciosVacios = 100 - contador;
 
+
+    // ----- Contador Espacios Vacíos -----
+    public static int contadorEspaciosVacios(){
+        int contadorEspaciosVacios  = coleccionVinilos[0].length - contadorVinilos();
         return contadorEspaciosVacios;
     }
+
 
 
     // ----- MENU -----
@@ -96,35 +114,28 @@ public class Main {
         int opcion;
 
         do{
-            System.out.println("==========================");
-            System.out.println("           MENÚ           ");
-            System.out.println("     COLECCIÓN VINILOS    ");
-            System.out.println("==========================");
-            System.out.println(" [1] Agregar vinilo       ");
-            System.out.println(" [2] Mostrar colección    ");
-            System.out.println(" [3] Buscar vinilo        ");
-            System.out.println(" [4] Salir                ");
-            System.out.println("==========================");
+            System.out.println("============================");
+            System.out.println("|           MENÚ           |");
+            System.out.println("|     COLECCIÓN VINILOS    |");
+            System.out.println("|==========================|");
+            System.out.println("| [1] Agregar vinilo       |");
+            System.out.println("| [2] Mostrar colección    |");
+            System.out.println("| [3] Buscar vinilo        |");
+            System.out.println("| [4] Salir                |");
+            System.out.println("============================");
             System.out.print  ("    Opción: ");
             opcion = lector.nextInt();
-            System.out.println("==========================");
+            System.out.println("|==========================|\n");
 
             switch (opcion){
-                case 1:
-                    agregarVinilo();
-                    break;
-                case 2:
-                    mostrarColeccion();
-                    break;
-                case 3:
-                    buscarVinilo();
-                    break;
-                case 4:
+                case 1 -> agregarVinilo();
+                case 2 -> mostrarColeccion();
+                case 3 -> buscarVinilo();
+                case 4 -> {
                     System.out.println(" Hasta luego... ");
                     System.exit(0);
-                    break;
-                default:
-                    System.out.println(" Ingrese una opción válida... ");
+                }
+                default -> System.out.println(" Ingrese una opción válida... ");
             }
         } while ( opcion != 4 );
     }
